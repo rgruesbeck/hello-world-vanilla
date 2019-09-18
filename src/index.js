1e8d7582699b470c282bbf35466b5ba1224e18ee
Ron Gruesbeck <rongruesbeck@gmail.com>
2019-09-18 13:49:33 -0700

    initial sketch

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// index.js
+
+function render() {
+    document.body.innerHTML = `
+    <h1>
+        ${Koji.config.settings.name}
+    </h1>
+    `
+}
+
+render();
---
+++ src/modules.js
@@ -0,0 +1,5 @@
+// modules.js is the entry point for modules from npm
+// import and require are supported
+
+import Koji from '@withkoji/vcc'; // import @withkoji/vcc
+window.Koji = Koji; // make Koji globally available
